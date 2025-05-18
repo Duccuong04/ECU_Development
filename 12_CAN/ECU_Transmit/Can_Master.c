b87ef0e77653be274a5d5d2f2f65d3aa00b768e0
Duccuong04 <duccuonggg04@gmail.com>
2025-05-18 14:52:27 +0700

    update CAN

--- 12_CAN/ECU_Transmit/Can_Master.c
+++ 12_CAN/ECU_Transmit/Can_Master.c
@@ -3,7 +3,7 @@
 #include "stm32f10x_gpio.h"             // Keil::Device:StdPeriph Drivers:GPIO
 #include "stm32f10x_can.h"              // Keil::Device:StdPeriph Drivers:CAN
 
-uint8_t dataTransmit[10] = {0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80, 0x90, 0xA0};
+uint8_t dataTransmit[10] = {0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80, 0x90, 0xA0}; // truyen duoc 8 byte thoi
 
 void RCC_Config(){
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
@@ -35,7 +35,7 @@ void CAN_Config(){
 	CAN_InitStructure.CAN_NART = DISABLE;  // No Automatic Retransmission
 	CAN_InitStructure.CAN_RFLM = DISABLE;  // Receive FIFO Locked Mode
 	CAN_InitStructure.CAN_TXFP = ENABLE;   // Transmit FIFO Priority
-	CAN_InitStructure.CAN_Mode = CAN_Mode_Normal;  // Ch? d? ho?t d?ng bình thu?ng
+	CAN_InitStructure.CAN_Mode = CAN_Mode_Normal;  
 
 	// C?u hình th?i gian truy?n (bit timing)
 	CAN_InitStructure.CAN_SJW = CAN_SJW_1tq;  // Synchronization Jump Width = 1 time quantum
@@ -48,20 +48,17 @@ void CAN_Config(){
 void CAN_TransmitData(uint8_t* data, uint8_t length){
 	CanTxMsg TxMessage;
 
-	TxMessage.StdId = 0x125;  // S? d?ng d?nh danh 11-bit
+	TxMessage.StdId = 0x103;  
 	TxMessage.RTR = CAN_RTR_Data;  // Data Frame
 	TxMessage.IDE = CAN_Id_Standard;  // Standard ID
-	TxMessage.DLC = length;  // S? byte d? li?u
+	TxMessage.DLC = length;  
 
-	// Copy d? li?u vào tru?ng d? li?u c?a khung
 	for (int i = 0; i < length; i++) {
 		TxMessage.Data[i] = data[i];
 	}
 	
-	//Transmit Message
 	uint8_t mailbox = CAN_Transmit(CAN1, &TxMessage);
 
-	// Ch? cho d?n khi khung du?c truy?n thành công
 	while (CAN_TransmitStatus(CAN1, mailbox) != CAN_TxStatus_Ok);
 }
 
@@ -69,7 +66,7 @@ int main(){
 	RCC_Config();
 	GPIO_Config();
 	CAN_Config();
-	CAN_TransmitData(dataTransmit, 7);
+	CAN_TransmitData(dataTransmit, 10);
 	while(1){
 	
 	}
---
+++ 12_CAN/readme.md
@@ -0,0 +1,3 @@
+![alt text](can-1.jpg)
+
+![alt text](<Screenshot 2025-05-18 131957-1.png>)
